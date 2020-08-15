6b75fdd080f6a5de2fac1114705f7573a57c5561
b856741 <b856741@yahoo.com.tw>
2020-08-16 03:17:52 +0800

    process

---
+++ 0730.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+   // $('h1').hide();
+   $('.title').hide();
+//    $('#title').hide();
+
+}
+);
+//
+$('.list-item').click(function (e) {
+    e.preventDefault();
+    // 請使用 toggleClass 完成此作業
+   })
---
+++ 0811daily/README.markdown
@@ -0,0 +1,5 @@
+# 0811daily
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/b856741/pen/ExKKPpr](https://codepen.io/b856741/pen/ExKKPpr).
+
+

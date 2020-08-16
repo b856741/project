b5455017809bd594c6feb8f75783eb5cb3e682c3
b856741 <b856741@yahoo.com.tw>
2020-08-17 00:30:30 +0800

    rename,daily

--- 0730.js
+++ 0730.js
@@ -1,12 +1,6 @@
-$(document).ready(function(){
-   // $('h1').hide();
-   $('.title').hide();
-//    $('#title').hide();
-
-}
-);
-//
 $('.list-item').click(function (e) {
-    e.preventDefault();
-    // 請使用 toggleClass 完成此作業
-   })
+   e.preventDefault();
+   $(this).toggleClass('active');
+ })
+ //對list-item中的元素active做切換
+ 
---
+++ jsp.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+//    $('h1').hide();
+   //$('.title').hide();//隱藏
+//    $('#title').hide();
+    $('h1').click(function(event) {
+      $('p').toggle();
+        //$('p').hide();  
+    });
+});
